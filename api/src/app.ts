10d6b48227de045b251c6444f43e01edf72efe13
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-20 12:20:43 +0530

    SETUP: graphql server

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import express from 'express';
+import cors from 'cors';
+import { graphqlHTTP } from 'express-graphql';
+import { log } from './utils/log';
+import { schema } from './graphql';
+
+const app = express();
+
+// apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use('/api', graphqlHTTP({ graphiql: true, schema })); // graphql hookup
+
+// start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  log.success(`🚀 SERVER STARTED ON PORT ${port}`);
+  log.info(`SERVER : http://localhost:${port}`);
+  log.info(`GRAPHQL: http://localhost:${port}/api`);
+});
