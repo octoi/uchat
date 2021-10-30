8efaa4e97c037970ba2574726adabf177cfa5f8a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-30 07:13:04 +0530

    configuration

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -17,7 +17,9 @@ export default function Layout({ children, title, description }: Props) {
           content={description ? description : 'Chut upp :)'}
         />
       </Head>
-      <main className='bg-gray-900 h-screen'>{children}</main>
+      <main className='bg-gray-900 bg-opacity-95 text-gray-200  h-screen'>
+        {children}
+      </main>
     </div>
   );
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.coolGray,
+      green: colors.teal,
+    },
   },
   variants: {
     extend: {},
