8cefdb443112a85ead35459e64c3aca77c591e23
fadhilsaheer <fadhilsaheer@gmail.com>
2021-11-01 15:09:45 +0530

    login user

--- app/graphql/account/account.mutation.ts
+++ app/graphql/account/account.mutation.ts
@@ -21,3 +21,15 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation ($email: String, $password: String) {
+    loginUser(email: $email, password: $password) {
+      id
+      email
+      name
+      profile
+      token
+    }
+  }
+`;
