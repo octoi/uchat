5305a960be5c0af53e189ac54602edee996a836b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-12 09:36:43 +0530

    redis host support

--- api/src/redis/index.ts
+++ api/src/redis/index.ts
@@ -2,8 +2,11 @@ import redis from 'redis';
 import { log } from '../utils/log';
 
 const REDIS_URL = process.env.REDIS_URL;
+const REDIS_HOST = process.env.REDIS_HOST;
 
-export const client = REDIS_URL
+export const client = REDIS_HOST
+  ? redis.createClient({ host: REDIS_HOST })
+  : REDIS_URL
   ? redis.createClient({ url: REDIS_URL })
   : redis.createClient();
 
